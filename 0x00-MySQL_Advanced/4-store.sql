e0d43aa9458e014c979fb3f23b575ee1ff07b697
Adewole Conde <phatboislym@protonmail.com>
2023-04-06 19:53:26 +0100

    fix: alternate approach

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,13 +2,7 @@
 -- quantity in the table items can be negative.
 -- Context: Updating multiple tables for one action from your application can generate issues
 -- network disconnection, crash, etc… to keep your data in a good shape, let MySQL do it for you!
-DELIMITER $$
 CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-  UPDATE items
-  SET quantity = quantity - NEW.quantity
-  WHERE id = NEW.item_id;
-END$$
-DELIMITER ;
-
+UPDATE items SET quantity = quantity - NEW.quantity
+WHERE items.id = NEW.item_id;
