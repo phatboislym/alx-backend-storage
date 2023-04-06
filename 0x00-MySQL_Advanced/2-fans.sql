720c1863436783b7dc0c468e80a244c7221ca0ac
Adewole Conde <phatboislym@protonmail.com>
2023-04-06 18:22:54 +0100

    fix: optimization changes

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -5,10 +5,8 @@
 -- Your script can be executed on any database
 SELECT origin, COUNT(*) AS nb_fans
 FROM (
-    SELECT DISTINCT band, fan
+    SELECT DISTINCT band, fan, origin
     FROM metal_bands
-) AS distinct_fans
-JOIN metal_bands ON distinct_fans.band = metal_bands.band AND distinct_fans.fan = metal_bands.fan
+) AS fan_origin
 GROUP BY origin
 ORDER BY nb_fans DESC;
-
