d48e0191dcaa755dc83c4ad18d3d0fcd2139f6fc
Adewole Conde <phatboislym@protonmail.com>
2023-04-07 00:52:29 +0100

    add: task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- SQL script that creates a trigger that resets the attribute valid_email
+-- only when the email has been changed
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF (OLD.email != NEW.email) THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+
