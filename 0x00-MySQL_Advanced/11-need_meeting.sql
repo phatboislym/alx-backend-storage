00e2c1aded56d335f91686c14ae63ff12c889d5e
Adewole Conde <phatboislym@protonmail.com>
2023-04-07 04:08:46 +0100

    add stubs

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,4 @@
+-- SQL script that creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month.
+-- Requirements:
+-- The view need_meeting should return all students name when:
+-- They score are under (strict) to 80
