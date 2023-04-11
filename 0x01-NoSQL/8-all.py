582aff9603254139edc1724663d66d4b2a751aed
Adewole Conde <phatboislym@protonmail.com>
2023-04-12 00:00:01 +0100

    add: task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+module for function `list_all`
+takes a pymongo collection object `mongo_collection`
+returns a list of all documents in a collection
+        or an empty list if there is no document in the collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    args: mongo_collection: pymongo.cursor.Cursor object
+    return: collection: list
+    """
+    collection = []
+    for mongo in mongo_collection:
+        collection.append(mongo)
+    return (collection)
