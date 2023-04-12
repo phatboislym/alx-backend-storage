e589a69996bbafd0b98623839ebd7751dea14574
Adewole Conde <phatboislym@protonmail.com>
2023-04-12 15:30:39 +0100

    fix: minor tweaks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -31,6 +31,6 @@ class Cache():
         args: data: Union[str, bytes, int, float])
         return: key: str
         """
-        random_key = str(uuid4())
+        random_key: str = str(uuid4())
         self._redis.set(random_key, data)
         return (random_key)
