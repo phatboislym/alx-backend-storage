31576d5fdf4cc028ee2306ed3c6a43786ad8e418
Adewole Conde <phatboislym@protonmail.com>
2023-04-12 15:10:49 +0100

    fix: call UUID4 method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,7 +10,7 @@ from typing import Union
 from uuid import uuid4
 
 
-class Cache:
+class Cache():
     """
     methods:
         __self__
@@ -31,6 +31,6 @@ class Cache:
         args: data: Union[str, bytes, int, float])
         return: key: str
         """
-        random_key = str(uuid4)
+        random_key: str = str(uuid4())
         self._redis.set(random_key, data)
         return (random_key)
