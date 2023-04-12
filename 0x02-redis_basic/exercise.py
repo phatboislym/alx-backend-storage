f45b193c1150dc51e4264ecc93691a753f5329ec
Adewole Conde <phatboislym@protonmail.com>
2023-04-12 15:08:21 +0100

    fix: remove typing for random_key variable

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,7 +20,7 @@ class Cache:
     def __init__(self) -> None:
         """
         stores an instance of the Redis client as private variable `_redis`
-        flushs the instance using flushdb
+        flushes the instance using flushdb
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -31,6 +31,6 @@ class Cache:
         args: data: Union[str, bytes, int, float])
         return: key: str
         """
-        random_key: str = str(uuid4)
+        random_key = str(uuid4)
         self._redis.set(random_key, data)
         return (random_key)
